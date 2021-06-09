e2bb84555e2462197fb929376cdd4e9eb919e05a
Mohak Garg <mohakgargmohak@gmail.com>
2021-06-09 10:44:12 +0530

    run

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(cors())
 
 
 app.use("/users", userRoutes);
-mongoose.connect('mongodb://localhost:27017/users', { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+mongoose.connect('mongodb://localhost:27017/user-management', { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log("Database connected successfully");
 });
 
